33f0624c2df806edcac80a9fcbccc5c56ec0992d
Derek Price <derekprice02@gmail.com>
2018-06-05 22:03:34 -0500

    Adding GET, PUT, DELETE

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,5 +1,19 @@
+var ObjectID = require('mongodb').ObjectID;
+
 module.exports = (app, db) => {
   const collection = db.collection('notes');
+
+  app.get('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id)};
+    collection.findOne(details, (err, item) => {
+      if (err) {
+        res.send({ 'error': 'An error has occurred.' });
+      } else {
+        res.status(200).send(item);
+      }
+    });
+  });
 
   app.post('/notes', (req, res) => {
     const note = {
@@ -12,6 +26,35 @@ module.exports = (app, db) => {
         res.send({ 'error': 'An error has occurred.' });
       } else {
         res.status(201).send(result.ops[0]);
+      }
+    });
+  });
+
+  app.put('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    const note = { text: req.body.body, title: req.body.title };
+    collection.update(details, note, (err, result) => {
+      if (err) {
+        res.send({
+          'error': 'An error has occurred.'
+        });
+      } else {
+        res.status(200).send(note);
+      }
+    });
+  });
+
+  app.delete('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id)};
+    collection.remove(details, (err, item) => {
+      if (err) {
+        res.send({ 'error': 'An error has occurred.' });
+      } else {
+        res.status(202).send({
+          "message": "Note " + id + " has been deleted."
+        });
       }
     });
   });
